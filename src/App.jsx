c2ee2b7f0a2ca40453d52b977d0cec041c382a01
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-19 22:07:15 +0600

    Applying tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <div className="bg-yellow-500">
+      <div className="">
        <div><Navbar/></div>
-       <div className="w-full h-screen mt-10">
+       <div className="w-full h-screen mt-10 text-white bg-black">
         <h1 className="text-white text-9xl">Hello React js</h1>
        </div>
         
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 
 function Navbar() {
   return (
-   <nav className="flex bg-amber-500 p-10 text-bold text-white mb-4">
-        <ul>
-            <li className="text-sm">Home</li>
-            <li className="text-lg">About</li>
-            <li className="text-xl">Contact</li>
+   <nav className=" rounded-3xl  bg-red-200 p-3 text-bold text-white">
+        <ul className="flex justify-center items-center">
+            <li className="text-sm p-3 hover:bg-green-500 transition duration-300">Home</li>
+            <li className="text-lg p-3 hover:bg-green-500 transition duration-300">About</li>
+            <li className="text-xl p-3 hover:bg-green-500 transition duration-300">Contact</li>
         </ul>
    </nav>
   )
