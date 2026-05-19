78e57e89fd3cf36a8ac6b612f3b1ebbe027a4b46
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-19 22:08:30 +0600

    Applying tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,11 @@ function Navbar() {
             <li className="text-sm p-3 hover:bg-green-500 transition duration-300">Home</li>
             <li className="text-lg p-3 hover:bg-green-500 transition duration-300">About</li>
             <li className="text-xl p-3 hover:bg-green-500 transition duration-300">Contact</li>
+            <button className="mt-5 bg-white text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300">
+
+          Click Me
+
+        </button>
         </ul>
    </nav>
   )
