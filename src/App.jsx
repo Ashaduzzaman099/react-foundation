0178bc71c118023c0dbd68e350b15bbc254db401
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-19 22:14:17 +0600

    Add sidebar Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <div className="">
-       <div><Navbar/></div>
-       <div className="w-full h-screen mt-10 text-white bg-black">
+       <div>
+        <Navbar/>
+      </div>
+      <div>
+        <Sidebar/>
+      </div>
+      <div className="w-full h-screen mt-10 text-white bg-black">
         <h1 className="text-white text-9xl">Hello React js</h1>
-       </div>
+      </div>
         
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@
 function Navbar() {
   return (
    <nav className=" rounded-3xl  bg-red-200 p-3 text-bold text-white">
-        <ul className="flex justify-center items-center">
+        <ul className="flex justify-center items-center ">
             <li className="text-sm p-3 hover:bg-green-500 transition duration-300">Home</li>
             <li className="text-lg p-3 hover:bg-green-500 transition duration-300">About</li>
             <li className="text-xl p-3 hover:bg-green-500 transition duration-300">Contact</li>
-            <button className="mt-5 bg-white text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300">
+            <button className=" bg-white text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300">
                 Click Me
             </button>
         </ul>
