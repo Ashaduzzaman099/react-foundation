dcf7d9503a75fadaa03ab84deb2e599df754c18b
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-20 21:56:51 +0600

    Use State to make counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import {useState} from "react";
 import UserCard from "./components/UserCard";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
       <div className="">
@@ -13,6 +14,8 @@ function App() {
         <UserCard name="Ashik khan" role="Admin"/>
         <UserCard name="Shakila Akter" role="User"/>
       </div>
+      <h1>{count}</h1>
+      <button className="bg-black text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300" onClick={()=> setCount(count+1)}>+</button>
         
       </div>
     </>
