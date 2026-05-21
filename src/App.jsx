af33a4d8cb8b3bad5b552dcec03840e01f74ee1f
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-21 15:37:26 +0600

    Update + - btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import {useState} from "react";
+import { useState } from "react";
 import UserCard from "./components/UserCard";
 
 function App() {
@@ -7,17 +7,26 @@ function App() {
   return (
     <>
       <div className="">
-       <div>
-        <Navbar/>
-      </div>
-      <div className="flex justify-center items-center">
-        <UserCard name="Ashik khan" role="Admin"/>
-        <UserCard name="Shakila Akter" role="User"/>
-      </div>
-      <h1>{count}</h1>
-      <button className="bg-black mr-2 text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300" onClick={()=> setCount(count+1)}>++</button>
-      <button className="bg-black text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300" onClick={()=> setCount(count-1)}>--</button>
-        
+        <div>
+          <Navbar />
+        </div>
+        <div className="flex justify-center items-center">
+          <UserCard name="Ashik khan" role="Admin" />
+          <UserCard name="Shakila Akter" role="User" />
+        </div>
+        <h1>{count}</h1>
+        <button
+          className="text-2xl bg-black mr-2 text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          className="text-2xl bg-black text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
       </div>
     </>
   );
