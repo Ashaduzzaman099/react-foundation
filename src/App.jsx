de628f97cba2b5d521e2ec7dc0db31175265b428
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-22 19:24:38 +0600

    Map function with react js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import { useState } from "react";
 import UserCard from "./components/UserCard";
 
+const user = ["Ashik khan", "Shakila Akter", "Rafiq", "Karim", "Jamal"];
+
 function App() {
   const [count, setCount] = useState(0);
   return (
@@ -14,19 +16,28 @@ function App() {
           <UserCard name="Ashik khan" role="Admin" />
           <UserCard name="Shakila Akter" role="User" />
         </div>
-        <h1>{count}</h1>
-        <button
-          className="text-2xl bg-black mr-2 text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300"
-          onClick={() => setCount(count + 1)}
-        >
-          +
-        </button>
-        <button
-          className="text-2xl bg-black text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300"
-          onClick={() => setCount(count - 1)}
-        >
-          -
-        </button>
+        <div>
+          <h1>{count}</h1>
+          <button
+            className="text-2xl bg-black mr-2 text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300"
+            onClick={() => setCount(count + 1)}
+          >
+            +
+          </button>
+          <button
+            className="text-2xl bg-black text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300"
+            onClick={() => setCount(count - 1)}
+          >
+            -
+          </button>
+        </div>
+        <div className="newSection">
+          {
+            user.map((u)=>(
+              <h1>{u}</h1>
+            ))
+          }
+        </div>
       </div>
     </>
   );
