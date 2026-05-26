0e60b2943480a860b6c6a178700216f5e4db63b2
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-26 19:05:30 +0600

    Change Color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 function Navbar() {
   return (
-    <nav className="bg-red-200 p-3 text-bold text-white">
+    <nav className="bg-green-500 p-3 text-bold text-white">
       <ul className="flex justify-center items-center ">
-        <li className="text-sm p-3 hover:bg-green-500 transition duration-300">
+        <li className="text-sm p-3 hover:bg-black  transition duration-300">
           Home
         </li>
-        <li className="text-lg p-3 hover:bg-green-500 transition duration-300">
+        <li className="text-lg p-3 hover:bg-black transition duration-300">
           About
         </li>
-        <li className="text-xl p-3 hover:bg-green-500 transition duration-300">
+        <li className="text-xl p-3 hover:bg-black  transition duration-300">
           Contact
         </li>
         <button className=" bg-white text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300">
