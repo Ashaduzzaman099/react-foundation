34b341301f60e780010a63ad42d52309e7237b3e
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-05-30 07:48:06 +0600

    Write new  hook

---
+++ src/components/useFetch.jsx
@@ -0,0 +1,18 @@
+import {useState,useEffect, use} from 'react'
+
+function useFetch(url) {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch(url)
+        .then(res => res.json())
+        .then(result => {
+            setData(result)
+            setLoading(false)
+        })
+},[url])
+
+    return {data, loading}
+
+export default useFetch
