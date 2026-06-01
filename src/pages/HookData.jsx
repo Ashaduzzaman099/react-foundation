62aae30749fd66b68d11ce6aba8f064c6331e589
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-01 15:53:07 +0600

    React Router Dom Implement

--- src/pages/HookData.jsx
+++ src/pages/HookData.jsx
@@ -1,20 +1,25 @@
-import useFetch from "../components/useFetch"
-import UserCard from "../components/UserCard"
+import useFetch from "../components/useFetch";
+import UserCard from "../components/UserCard";
 
 const HookData = () => {
-    const {data, loading}= useFetch('https://jsonplaceholder.typicode.com/users')
-    if(loading){
-        return <h1>Loading...</h1>
-    }
-    return (
+  const { data, loading } = useFetch(
+    "https://jsonplaceholder.typicode.com/users",
+  );
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <div className="flex mt-5 p-5 justify-center items-center flex-wrap gap-5 bg-gray-500 pt-20 pb-20">
+      {data.map((user) => (
+        <UserCard
+          key={user.id}
+          name={user.name}
+          email={user.email}
+          role={user.role}
+        />
+      ))}
+    </div>
+  );
+};
 
-        <div className="flex mt-5 p-5 justify-center items-center flex-wrap gap-5 bg-gray-500 pt-20 pb-20">
-            {
-            data.map(user => <UserCard key={user.id} name={user.name} email={user.email} role={user.role} />)
-            }
-        </div>
-
-    )
-}
-
-export default HookData
+export default HookData;
