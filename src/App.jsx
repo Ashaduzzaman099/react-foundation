8c3e1e5fb75a10cdf6c2a4742c478d27693d8a34
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-02 15:08:45 +0600

    React Router Dom Implement

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/about" element={<About />} />
-
         <Route path="/contact" element={<Contact />} />
         <Route path="/data" element={<HookData />} />
       </Routes>
