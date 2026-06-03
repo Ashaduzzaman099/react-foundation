a652f8575656f19c71fe0ebab8a9a25931d1ace0
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-03 13:09:42 +0600

    Link to other pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
       <nav className="bg-green-500 p-3 text-bold text-white">
-        <ul className="flex justify-center items-center ">
-          <li className="text-sm p-3 hover:bg-black  transition duration-300">
-            Home
-          </li>
-          <li className="text-lg p-3 hover:bg-black transition duration-300">
-            About
-          </li>
-          <li className="text-xl p-3 hover:bg-black  transition duration-300">
-            Contact
-          </li>
-          <button className=" bg-white text-blue-500 px-5 py-2 rounded-xl hover:bg-black hover:text-white transition duration-300">
-            Click Me
-          </button>
-        </ul>
+        <Link to="/">Home</Link>
+        {" | "}
+        <Link to="/about">About</Link>
+        {" | "}
+        <Link to="/contact">Contact</Link>
       </nav>
     </>
   );
